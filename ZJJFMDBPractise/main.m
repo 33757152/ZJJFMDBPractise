589853c61915d1f0b72621a95d35309ee62a1d58
张锦江 <zhangjinjiang@zhangjinjiangdeMacBook-Pro.local>
2017-07-11 16:24:48 +0800

    Initial Commit

---
+++ ZJJFMDBPractise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJJFMDBPractise
+//
+//  Created by 张锦江 on 2017/7/11.
+//  Copyright © 2017年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
